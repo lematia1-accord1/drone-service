c7cedbb357258fbdc93da977ea1412b23cd9899c
lematia1-accord1 <lematia1accord1@gmail.com>
2023-01-08 14:40:29 +0300

    updated the comments according to the review comments

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
-# import the required packages
+
 from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
-# init app
 app = Flask(__name__)
-# create a route for drone_service
 @app.route("/drone-service", methods = ["GET"])
 def get_message_from_drone():
     return jsonify({"message": "hellow"})
-# run the server
+
 if __name__ == "__main__":
     app.run(debug=True)
+    
